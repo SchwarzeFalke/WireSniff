0d4b1dfea7a01742224057af4efc21f6aa1cb0dd
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-01-25 13:32:30 -0600

    Fixing ascii bugs

--- script.py
+++ script.py
@@ -1,11 +1,9 @@
 # @Author: schwarze_falke
-# @Date:   2019-01-25T11:14:07-06:00
+# @Date:   2019-01-25T13:30:28-06:00
 # @Last modified by:   schwarze_falke
-# @Last modified time: 2019-01-25T12:01:09-06:00
+# @Last modified time: 2019-01-25T13:32:08-06:00
+import binascii
 
-with open("ethernet_1", "rb") as f:
-    byte = f.read(1)
-    while byte:
-        # Do stuff with byte.
-        byte = f.read(1)
-        print(byte)
+file = open("ethernet_1.bin", "rb")
+byte = file.read()
+print(bin(int(binascii.hexlify(byte),16)))
