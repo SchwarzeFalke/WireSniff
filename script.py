8dd652b661f6bbf4c4fe9e0ab70b4a8d7015b539
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-01-25 13:44:09 -0600

    Adding hexConvert method to the program

--- script.py
+++ script.py
@@ -1,9 +1,16 @@
 # @Author: schwarze_falke
 # @Date:   2019-01-25T13:30:28-06:00
 # @Last modified by:   schwarze_falke
-# @Last modified time: 2019-01-25T13:32:08-06:00
+# @Last modified time: 2019-01-25T13:43:39-06:00
 import binascii
 
-file = open("ethernet_1.bin", "rb")
-byte = file.read()
-print(bin(int(binascii.hexlify(byte),16)))
+def menu():
+    file = open("ethernet_1.bin", "rb")
+    binString = bin(int(binascii.hexlify(file.read()),16))
+    file.close()
+    print(hexConvert(binString[4:8]))
+
+def hexConvert(varString):
+    return hex(int(varString, 2))[2:]
+
+menu()
