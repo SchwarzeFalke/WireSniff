358db1093d7929f678e233516c5ce65ec13d92d3
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-01-25 14:50:43 -0600

    Adding condition for the type of data

--- script.py
+++ script.py
@@ -1,28 +1,38 @@
 # @Author: schwarze_falke
 # @Date:   2019-01-25T13:30:28-06:00
 # @Last modified by:   schwarze_falke
-# @Last modified time: 2019-01-25T14:35:15-06:00
+# @Last modified time: 2019-01-25T14:50:31-06:00
 import binascii
 
+# Main function
 def menu():
-    file = open("ethernet_1.bin", "rb")
-    hexString = binascii.hexlify(file.read()).upper()
+    file = open(input('Ingrese el nombre del archivo a leer: '), "rb")
+    hexString = binascii.hexlify(file.read()).upper()       # All file's data is read an process as hex
     file.close()
 
-    originAddress = formatString(hexString[0:12], 14)
-    destinationAddress = formatString(hexString[12:24], 14)
-    type = hexString[24:28]
-    ip = formatString(hexString[28:68], 42)
-    tcp = formatString(hexString[68:114], 48)
+    originAddress = formatString(hexString[0:12], 14)       # The origin address has a lenght of 6 bytes
+    destinationAddress = formatString(hexString[12:24], 14) # also the destination address; so, 6x2 = 12
+    type = hexString[24:28]                                 # The type information has a lenght of 2 bytes
+    ip = formatString(hexString[28:68], 42)                 # IP has a lenght of 20 bytes
+    tcp = formatString(hexString[68:114], 48)               # TCP's lenght is 23
     data = formatString(hexString[114:len(hexString)], (len(hexString)-112))
 
     print "Direccion MAC de origen: ", originAddress
     print "Direccion MAC de destino: ", destinationAddress
-    print "Tipo: ", type
     print "IP: ", ip
     print "TCP: ", tcp
     print "Datos: ", data
+    if type == '0800':
+        print "Tipo: ", type, " (IPv4)"
+    if type == '0806':
+        print "Tipo: ", type, " (ARP)"
+    if type == '8035':
+        print "Tipo: ", type, " (RARP)"
+    if type == '08DD':
+        print "Tipo: ", type, " (IPv6)"
 
+# This function gives a string a defined format of the type "00:00:00"
+# depending on the lenght [given by @top]. It returns a formatted string
 def formatString(varString, top):
     finalString = ''
     a = 0
@@ -33,7 +43,6 @@ def formatString(varString, top):
         b += 2
         if(b != top):
             finalString += ':'
-
     return finalString
 
 menu()
