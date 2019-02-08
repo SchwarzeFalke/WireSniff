0f970d66953f6a3c5bbb1d0906b8398d2bbe80e8
Carlos Vara <carlos.vara@alumnos.udg.mx>
2019-02-08 11:40:55 -0600

    Adding version of IP protocol

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 # @Author: schwarze_falke
 # @Date:   2019-01-25T13:30:28-06:00
 # @Last modified by:   schwarze_falke
-# @Last modified time: 2019-02-08T11:35:52-06:00
+# @Last modified time: 2019-02-08T11:39:22-06:00
 import binascii
 
 # Main function
@@ -20,7 +20,15 @@ def menu():
         print "Tipo: ", type, " (IPv4)"
         ip = bin(int(hexString[28:68], 16))[4:]  # IP has a lenght of 20 bytes
         print "IP: ", ip
-        
+        version = ip[0:4]
+        header = ip[4:4]
+        header = int(header)
+        print header
+        if version == "0010":
+            print "Version: IPv4"
+        elif version == "0100":
+            print "Version: IPv6"
+
     if type == '0806':
         ip = formatString(hexString[28:68], 42)  # IP has a lenght of 20 bytes
         tcp = formatString(hexString[68:114], 48)               # TCP's lenght is 23
